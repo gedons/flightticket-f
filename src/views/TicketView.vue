507f6072a620e320a8844373a787015064c8e5c4
Gedoni Ani <gedoniani@gmail.com>
2025-09-09 10:31:59 +0100

    update

--- src/views/TicketView.vue
+++ src/views/TicketView.vue
@@ -145,7 +145,7 @@
                   <svg class="w-4 h-4 text-gray-400 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clip-rule="evenodd" />
                   </svg>
-                  <span class="text-gray-600">Passport: {{ p.passport || 'Not provided' }}</span>
+                  <span class="text-gray-600">Contact: {{ p.passport || 'Not provided' }}</span>
                 </div>
               </div>
             </div>
